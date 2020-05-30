dfb498e6801ddaaabe5f94ab56f83deb7d6c1e4c
khizer.khan98@gmail.com <khizer.khan98@gmail.com>
2020-05-30 21:44:06 +0500

    Reverting back to Whitenoise Compressed Manifest

--- blog/settings.py
+++ blog/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR , "assets")
 
